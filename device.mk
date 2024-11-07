2327cd487b7919fc8c944887eae7299d73f74497
ReveRTX <rev9rt@gmail.com>
2024-11-07 21:19:02 +0530

    Revert "veux: Welcome to VoltageOS"
    This reverts commit 1c29af726bfad7241854c62bf699a3c2bbe5bb09.

--- device.mk
+++ device.mk
@@ -234,10 +234,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCOMMON \
+    DialerOverlayCOMMON \
     TelephonyOverlayCOMMON
 
 PRODUCT_PACKAGES += \
     FrameworkOverlayVEUX \
+    LineageSDKOverlayVEUX \
     SettingsOverlayVEUX \
     SystemUIOverlayVEUX \
     WifiOverlayVEUX
